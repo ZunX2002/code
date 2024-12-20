263bfcc9ce301923f6ceef569291ffbdc5ce3ec7
Nguyễn Văn Dũng <dungtshhbg20022@gmail.com>
2024-12-20 20:03:01 +0700

    add file

---
+++ FoodOrderBasicJava/app/src/main/java/com/example/foodorder/constant/AboutUsConfig.java
@@ -0,0 +1,18 @@
+package com.example.foodorder.constant;
+
+public interface AboutUsConfig {
+    String ABOUT_US_TITLE = "ZunX";
+    String ABOUT_US_CONTENT = "Provides Android, iOS, Web Source Codes and Development Services. " +
+            "Highly Committed to Quality Products and Excellent Services.";
+    String ABOUT_US_SLOGAN = "Highly committed to quality products & excellent services";
+    String ABOUT_US_WEBSITE_TITLE = "Website TinCoder";
+
+    String PAGE_FACEBOOK = "fb://page/tincoder";
+    String LINK_FACEBOOK = "https://www.facebook.com/";
+    String LINK_YOUTUBE = "";
+    String PHONE_NUMBER = "+84392229676";
+    String GMAIL = "dungtshhbg2002@gmail.com";
+    String SKYPE_ID = "";
+    String ZALO_LINK = "https://zalo.me/0333819210";
+    String WEBSITE = "https://alabaster-drop-b52.notion.site/TinCoder-Full-Source-Code-Android-c1f9d9d224004932823b6d5061c80dbc";
+}
